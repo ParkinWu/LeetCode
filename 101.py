933699168ea9709776abe4d071178e44cc0a0ab2
ParkinWu <snowy_way@163.com>
2019-06-26 13:34:19 +0800

    101

---
+++ 101.py
@@ -0,0 +1,48 @@
+# 给定一个二叉树，检查它是否是镜像对称的。
+#
+# 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+#
+#     1
+#    / \
+#   2   2
+#  / \ / \
+# 3  4 4  3
+# 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+#
+#     1
+#    / \
+#   2   2
+#    \   \
+#    3    3
+# 说明:
+#
+# 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/symmetric-tree
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def isMirror(self, t1: TreeNode, t2: TreeNode) -> bool:
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+        return t1.val == t2.val and self.isMirror(t1.left, t2.right) and self.isMirror(t2.left, t1.right)
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.isMirror(root, root)
+
+
+
+
+
+
