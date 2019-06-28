e6581c952da1b6e7c7677bcd9b9ff13c0fc1ea40
ParkinWu <snowy_way@163.com>
2019-06-28 18:34:24 +0800

    ac 77

---
+++ 77.py
@@ -0,0 +1,40 @@
+# 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+#
+# 示例:
+#
+# 输入: n = 4, k = 2
+# 输出:
+# [
+#   [2,4],
+#   [3,4],
+#   [2,3],
+#   [1,2],
+#   [1,3],
+#   [1,4],
+# ]
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/combinations
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+from typing import List
+
+
+class Solution:
+    def select(self, nums: List[int], n: int) -> List[List[int]]:
+        if n == 0 or len(nums) < n:
+            return [[]]
+        one = nums[:1]
+        rest = self.select(nums[1:], n - 1)
+        return list(map(lambda x: one + x, rest)) + self.select(nums[1:], n)
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = [i for i in range(1, n + 1)]
+        return list(filter(lambda l: len(l) == k, self.select(nums, k)))
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.combine(1, 1)
+    print(r)
+
