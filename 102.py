72ad3da650cb58aa8bba3e788c9075bdd4ece0be
ParkinWu <snowy_way@163.com>
2019-07-09 12:58:06 +0800

    ac 102

---
+++ 102.py
@@ -0,0 +1,52 @@
+#
+# 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+#
+# 例如:
+# 给定二叉树: [3,9,20,null,null,15,7],
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# 返回其层次遍历结果：
+#
+# [
+#   [3],
+#   [9,20],
+#   [15,7]
+# ]
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        siblings = [root]
+        ans = [[root.val]]
+        while True:
+            tmp = []
+            for node in siblings:
+                if node.left:
+                    tmp.append(node.left)
+                if node.right:
+                    tmp.append(node.right)
+            if not tmp:
+                break
+            else:
+                ans.append(list(map(lambda n: n.val, tmp)))
+            siblings = tmp
+        return ans
+
+
+
+
