13459d7cb6f1c6bfcaa2d7624c38f0b59f48f22b
ParkinWu <snowy_way@163.com>
2020-02-08 20:53:24 +0800

    ac 354

---
+++ python/354.py
@@ -0,0 +1,44 @@
+# 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式 (w, h) 出现。当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+#
+# 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+#
+# 说明:
+# 不允许旋转信封。
+#
+# 示例:
+#
+# 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+# 输出: 3
+# 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/russian-doll-envelopes
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+from typing import List
+import bisect
+class Solution:
+    def lis(self, nums: List[int]) -> int:
+        tail = []
+        for num in nums:
+            insert_pos = bisect.bisect_left(tail, num)
+            if insert_pos == len(tail):
+                tail.append(num)
+            elif tail[insert_pos] > num:
+                tail[insert_pos] = num
+        return len(tail)
+
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        if not envelopes:
+            return 0
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        return self.lis([i[1] for i in envelopes])
+
+
+if __name__ == '__main__':
+    s = Solution()
+    assert s.maxEnvelopes([[46, 89], [50, 53], [52, 68], [72, 45], [77, 81]]) == 3
+    assert s.maxEnvelopes(
+        [[2, 100], [3, 200], [4, 300], [5, 500], [5, 400], [5, 250], [6, 370], [6, 360], [7, 380]]) == 5
+    assert s.maxEnvelopes([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [5, 5], [6, 7], [7, 8]]) == 7
+    assert s.maxEnvelopes([[5, 4], [6, 4], [6, 7], [2, 3]]) == 3
+    assert s.maxEnvelopes([[1, 1], [1, 1], [1, 1]]) == 1
