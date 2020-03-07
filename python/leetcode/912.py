915c027941f0015eee6d6491eed2beb82fa8bd21
ParkinWu <snowy_way@163.com>
2020-03-07 15:55:05 +0800

    ac 168

--- python/leetcode/912.py
+++ python/leetcode/912.py
@@ -21,22 +21,47 @@
 # 链接：https://leetcode-cn.com/problems/sort-an-array
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 from typing import List
+
 class Solution:
 
-    def quicksort(self, nums: List[int], lo: int, hi: int) -> List[int]:
-        if len(nums) < 2:
-            return nums
-        key = nums[0]
-        left = []
-        right = []
-        for i in range(lo + 1, hi):
-            if nums[i] <= key:
-                left.append(nums[i])
-            else:
-                right.append(nums[i])
-        return self.quicksort(left, 0, len(left)) + [key] + self.quicksort(right, 0, len(right))
+    # def quicksort(self, nums: List[int], lo: int, hi: int) -> List[int]:
+    #     if len(nums) < 2:
+    #         return nums
+    #     key = nums[0]
+    #     left = []
+    #     right = []
+    #     for i in range(lo + 1, hi):
+    #         if nums[i] <= key:
+    #             left.append(nums[i])
+    #         else:
+    #             right.append(nums[i])
+    #     return self.quicksort(left, 0, len(left)) + [key] + self.quicksort(right, 0, len(right))
+
+    def partition(self, nums: List[int], lo: int, hi: int) -> int:
+        k = nums[lo]
+        while lo < hi:
+            while lo < hi and nums[hi] >= k:
+                hi -= 1
+            nums[lo] = nums[hi]
+            while lo < hi and nums[lo] <= k:
+                lo += 1
+            nums[hi] = nums[lo]
+        nums[lo] = k
+        return lo
+
+    def quicksort_inplace(self, nums: List[int], lo: int, hi: int):
+        if lo >= hi:
+            return
+        p = self.partition(nums, lo, hi)
+        self.quicksort_inplace(nums, lo, p - 1)
+        self.quicksort_inplace(nums, p + 1, hi)
 
     def sortArray(self, nums: List[int]) -> List[int]:
-        return self.quicksort(nums, 0, len(nums))
+        self.quicksort_inplace(nums, 0, len(nums) - 1)
+        return nums
+        # return self.quicksort(nums, 0, len(nums))
 
+if __name__ == '__main__':
+    s = Solution()
+    assert s.sortArray([5, 1, 1, 2, 0, 0]) == [0, 0, 1, 1, 2, 5]
 
